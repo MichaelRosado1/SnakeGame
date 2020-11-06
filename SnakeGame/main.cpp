5916d1a534f4849b7bffbbaa9785756fa10d4217
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-05 22:16:27 -0600

    Initial Commit

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SnakeGame
+//
+//  Created by Michael Rosado on 11/5/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
