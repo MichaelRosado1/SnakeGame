6526029fccce2b46d205e7844ac9823c1e7bc864
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-05 23:12:53 -0600

    Set up classes and small amount of work on render method

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -6,9 +6,52 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class Snake {
+    
+};
+
+class Apple {
+    
+};
+
+class Game {
+public:
+    int height = 1;
+    int width = 1;
+    
+    Game(int height, int width) {
+        this->height = height;
+        this->width = width;
+        
+    }
+    
+    void board_Layout() {
+      
+        char board_matrix[height][width];
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+               //TODO: fill in game board with spaces or '+','|', or '-' characters
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+    void render() {
+        std::cout<<"Height is: "<<height<<std::endl;
+        std::cout<<"width is: "<<width<<std::endl;
+    }
+};
+
+int main() {
+    
+    Game* game = new Game(10,20);
+    game->board_Layout();
+    
 }
