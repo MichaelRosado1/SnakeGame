27a7824f7b5882cca1fde46c532c606b490217aa
MichaelRosado1 <mrosad4@illinois.edu>
2020-11-06 23:20:26 -0600

    Added Snake class which deals with the positions of the snake using tuples.

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -7,8 +7,36 @@
 
 #include <iostream>
 #include <vector>
+#include <tuple>
+
+const std::tuple<int, int> UP {1,0};
+const std::tuple<int, int>DOWN {0,-1};
+const std::tuple<int, int>LEFT {-1,0};
+const std::tuple<int, int> RIGHT {1,0};
 
 class Snake {
+    
+public:
+    std::vector<std::tuple<int, int>> body;
+    std::tuple<int, int> direction;
+    
+    
+    Snake(std::vector<std::tuple<int, int>> bodyPos, std::tuple<int, int> startDirection) {
+        this->body = bodyPos;
+        this->direction = startDirection;
+    }
+    
+    void take_step(std::tuple<int, int> position) {
+        body.insert(body.begin(), position);
+        body.pop_back();
+    }
+    void set_direction(std::tuple<int, int> direction) {
+        this->direction = direction;
+    }
+    
+    std::tuple<int,int> head() {
+        return this->body.at(0);
+    }
     
 };
 
@@ -20,28 +48,25 @@ class Game {
 public:
     int height = 0;
     int width = 0;
-    
+    Snake *snake;
     Game(int height, int width) {
         this->height = height;
         this->width = width;
+        std::vector<std::tuple<int,int>> startingSnake{};
+        snake = new Snake(startingSnake, UP);
         
     }
     
-std::vector<std::vector<char>> board_Layout() {
+        std::vector<std::vector<char>> board_Layout() {
       
         std::vector<std::vector<char>> board_matrix(width, std::vector<char>(height, ' '));
         
+        std::tuple<int,int> headPosition = snake->head();
+        
+        std::vector<std::tuple<int, int>> bodyPositions = snake->body;
     
         for (int i = 0; i < board_matrix.size(); i++) {
             for (int j = 0; j < board_matrix[i].size(); j++) {
-                /*
-               (0,0)       (0, width - 1)
-                 +---------+
-                 |         |
-                 |         |
-                 +---------+
-                 
-                 */
                 if (i == 0 || i == board_matrix.size() - 1) {
                     board_matrix[i][j] = '*';
                 } else if (i != 0 && (j == 0 || j == board_matrix[i].size() - 1)) {
@@ -50,6 +75,11 @@ std::vector<std::vector<char>> board_Layout() {
                 
             }
         }
+            for (int i = 0; i <bodyPositions.size(); i++) {
+                board_matrix[get<0>(bodyPositions[i])][get<1>(bodyPositions[i])] = 'O';
+            }
+            board_matrix[get<0>(headPosition)][get<1>(headPosition)] = 'X';
+        
     return board_matrix;
         
     }
